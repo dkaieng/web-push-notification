26eed6802ab529c7957cf53ef7fd260419d16b84
Kien <kien.gian@citek.vn>
2023-10-03 17:11:29 +0700

    Updated 1

--- sw.js
+++ sw.js
@@ -1,8 +1,3 @@
 self.addEventListener('push', () => {
-  const options = {
-    // Định nghĩa các tùy chọn cho thông báo
-    body: 'Hello sir!',
-    // Các tùy chọn khác...
-  };
-  self.registration.showNotification('Hello world!', options);
+  self.registration.showNotification('Hello world!');
 });
